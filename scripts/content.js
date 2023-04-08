2489a8d3b9ed45543461f4fab444fd32d04cfc6d
Jiali Zhang <jzhang@Macintosh-2.local>
2023-04-08 13:02:27 -0700

    fix bug

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,11 @@
-function inject(num) {
+async function inject(num) {
 	const newIframe = document.createElement("iframe");
-	const courseInfo = document.querySelector("h1");
+	let courseInfo = document.querySelector("h1");
+	while (num < 20 && !courseInfo) {
+		await sleep(1000);
+		courseInfo = document.querySelector("h1");
+	}
 	if (!courseInfo) {
-		if (num < 20) {
-			setTimeout(() => {
-				inject(num + 1);
-			}, 1000);
-		}
 		return;
 	}
 	const courseName = courseInfo.textContent.split(" ").slice(0, 2);
@@ -16,6 +15,12 @@ function inject(num) {
 	newIframe.style.width = "100%";
 	const cdpCourseDetails = document.querySelector(".cdpCourseDetails");
 	cdpCourseDetails.appendChild(newIframe);
+}
+
+function sleep(delay) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, delay);
+	});
 }
 
 setTimeout(() => {
